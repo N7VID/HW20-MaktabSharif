fb1457ae6aa4f1e6bf71b2af752ec29310b757a9
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-19 00:02:33 +0330

    fix: homepage style

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,9 +1,22 @@
+import Button from "../../components/Button";
+
 export default function HomePage() {
   return (
     <div className="flex justify-center items-center h-screen bg-hero-homepage bg-center bg-cover cursor-default font-yekan">
-      <h1 className="flex justify-center items-center font-bold text-4xl">
-        وب اپلیکیشن مدیریت مخاطبین
-      </h1>
+      <div className="flex flex-col gap-14">
+        <h1 className="flex justify-center items-center font-bold text-5xl text-[#2C3E50]">
+          وب اپلیکیشن مدیریت مخاطبین
+        </h1>
+        <p className="text-center text-lg text-[#2C3E50] font-semibold leading-8">
+          مدیریت تماس‌ها و اطلاعات دوستان و همکاران خود را به سادگی انجام دهید.
+          <br />
+          همه چیز در یک جا، همیشه در دسترس.
+        </p>
+        <div className="flex gap-5 justify-around px-32">
+          <Button variant={"outline"}>ثبت نام</Button>
+          <Button variant={"solid"}>ورود</Button>
+        </div>
+      </div>
     </div>
   );
 }

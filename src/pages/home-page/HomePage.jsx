1b2e59cbc69003464470f15e24b5fc0a4faab31a
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-18 21:46:22 +0330

    feat: homepage style

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-hero-homepage bg-center bg-cover cursor-default">
+      <h1 className="flex justify-center items-center font-bold text-4xl">
+        وب اپلیکیشن مدیریت مخاطبین
+      </h1>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-homepage": "url('/public/hero-homepage.jpg')",
+      },
+    },
   },
   plugins: [],
 };

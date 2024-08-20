741a66a8a8228f4808a0dce4941d6ee7f4163c8d
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-20 21:04:31 +0330

    fix: style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,11 @@ export default function Header() {
   const user = useSelector(selectAuthUser);
   const dispatch = useDispatch();
   return (
-    <div className="bg-[#a9d9e2] w-full py-4 px-12 cursor-default flex justify-between items-center text-[#2C3E50]">
+    <div className="bg-[#edf8fb] w-full py-4 px-12 cursor-default flex justify-between items-center text-[#2C3E50]">
       <div className="flex items-center gap-4">
         <img src="/users.svg" alt="" className="w-7" />
         <h1 className="text-lg font-semibold">
-          وب اپلیکیشن مدیریت <span className="text-red-500">مخاطبین</span>
+          وب اپلیکیشن مدیریت <span className="text-blue-500">مخاطبین</span>
         </h1>
       </div>
       <div className="flex items-center justify-center gap-3">

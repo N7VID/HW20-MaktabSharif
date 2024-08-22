de29c9d16a868c4db60e6044014d1282b14fdd52
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-22 04:39:02 +0330

    fix: style single page

--- src/pages/contacts-single-page/ContactsSinglePage.jsx
+++ src/pages/contacts-single-page/ContactsSinglePage.jsx
@@ -14,39 +14,41 @@ export default function ContactsSinglePage() {
   }, [dispatch, id]);
 
   return (
-    <div className="bg-hero-main-2 min-h-[calc(100vh-60px)] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center font-yekan cursor-default">
-      <div className="flex-grow flex-col glassCard flex justify-center gap-8 items-center my-[40px] w-[430px]">
+    <div className="bg-hero-homepage min-h-[calc(100vh-60px)] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center font-yekan cursor-default">
+      <div className="flex-col glassCard flex justify-center gap-6 items-center pt-5 pb-7 my-[20px] w-[300px] tablet:w-[400px] desktop:w-[400px]">
         <h1 className="text-xl font-bold">اطلاعات مخاطب</h1>
-        <div className="w-[375px] relative">
+        <div className="w-[250px] desktop:w-[360px] tablet:w-[360px] relative">
           <img src={user?.imgSrc} alt="user-image" className="rounded-lg" />
           <Link to={"/contacts"}>
             <img
               src="/back.svg"
-              className="w-10 absolute top-1 left-1"
+              className="w-7 desktop:w-9 tablet:w-9 absolute top-1 left-1"
               alt="back-icon"
             />
           </Link>
         </div>
-        <div className="flex flex-col gap-2 text-lg">
-          <div className="flex gap-4 bg-white py-2 px-6 rounded-md justify-between w-[380px]">
+        <div className="flex flex-col gap-2 text-sm desktop:text-base tablet:text-base">
+          <div className="flex gap-4 bg-white py-2 px-3 tablet:px-6 desktop:px-6 rounded-md justify-between w-[250px] tablet:w-[360px] desktop:w-[340px]">
             <span className="font-semibold min-w-fit">نام و نام خانوادگی</span>
             <span className="whitespace-nowrap overflow-hidden text-ellipsis">
               {user?.fullName}
             </span>
           </div>
-          <div className="flex gap-4 bg-white py-2 px-6 rounded-md justify-between w-[380px]">
+          <div className="flex gap-4 bg-white py-2 px-3 tablet:px-6 desktop:px-6 rounded-md justify-between w-[250px] tablet:w-[360px] desktop:w-[340px]">
             <span className="font-semibold">شماره موبایل</span>
             <span>{user?.phoneNumber}</span>
           </div>
-          <div className="flex gap-4 bg-white py-2 px-6 rounded-md justify-between w-[380px]">
+          <div className="flex gap-4 bg-white py-2 px-3 tablet:px-6 desktop:px-6 rounded-md justify-between w-[250px] tablet:w-[360px] desktop:w-[340px]">
             <span className="font-semibold min-w-fit">ایمیل</span>
-            <span>{user?.email}</span>
+            <span className="whitespace-nowrap overflow-hidden text-ellipsis ltr">
+              {user?.email}
+            </span>
           </div>
-          <div className="flex gap-4 bg-white py-2 px-6 rounded-md justify-between w-[380px]">
+          <div className="flex gap-4 bg-white py-2 px-3 tablet:px-6 desktop:px-6 rounded-md justify-between w-[250px] tablet:w-[360px] desktop:w-[340px]">
             <span className="font-semibold">شغل</span>
             <span>{user?.job}</span>
           </div>
-          <div className="flex gap-4 bg-white py-2 px-6 rounded-md justify-between w-[380px]">
+          <div className="flex gap-4 bg-white py-2 px-3 tablet:px-6 desktop:px-6 rounded-md justify-between w-[250px] tablet:w-[360px] desktop:w-[340px]">
             <span className="font-semibold">گروه</span>
             <span>{user?.relative}</span>
           </div>
